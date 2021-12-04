00bee7ec8da0b8d5c25b8bfb26311e445726c578
panthamima <nastyadima2003@mail.ru>
2021-12-04 05:25:37 -0600

    rename folders

---
+++ P2P-CHAT/CMakeFiles/C_P2P_Chat.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C_P2P_Chat.dir/src/main.c.o"
+  "CMakeFiles/C_P2P_Chat.dir/src/chat.c.o"
+  "CMakeFiles/C_P2P_Chat.dir/src/clients.c.o"
+  "CMakeFiles/C_P2P_Chat.dir/src/network/socket.c.o"
+  "CMakeFiles/C_P2P_Chat.dir/src/network/packet.c.o"
+  "CMakeFiles/C_P2P_Chat.dir/src/utils/utils.c.o"
+  "CMakeFiles/C_P2P_Chat.dir/src/utils/interface.c.o"
+  "C_P2P_Chat.pdb"
+  "C_P2P_Chat"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/C_P2P_Chat.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
