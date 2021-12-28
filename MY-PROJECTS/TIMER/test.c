0268a9216c8422b7c2042dbe0cc5ff291126ee9d
panthamima <nastyadima2003@mail.ru>
2021-12-28 22:35:25 +0700

    centered timer v0.2

--- MY-PROJECTS/TIMER/test.c
+++ MY-PROJECTS/TIMER/test.c
@@ -5,31 +5,78 @@
 #include <unistd.h>
 #include <sys/ioctl.h>
 #include <unistd.h>
+#include <signal.h>
 #include <ncurses.h>
 #include <stdarg.h>
 
 
-int main (int argc, char **argv) {
-    struct winsize w;
-    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
-    int width = w.ws_col;
-    int heigth = w.ws_row;
-    int width05 = w.ws_col / 2;
-    char x[] = " "; 
-    int word_size = strlen(x);
+// int main (int argc, char **argv) {
+//     struct winsize w;
+//     ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
     
+//         int heigth = w.ws_row;
+//         int width = w.ws_col;
+//         int width05 = w.ws_col / 2;
+//         char x[] = " "; 
+//         int word_size = strlen(x);
 
-    while (heigth == 21 & width == 92) {
-        system("clear");
-        time_t t = time(NULL);
-        struct tm* aTm = localtime(&t);
-        // printf ("%02d:%02d:%02d\n",aTm->tm_hour, aTm->tm_min, aTm->tm_sec);
-        printf("%d %02d:%02d:%02d %d", width05 ,aTm->tm_hour, aTm->tm_min, aTm->tm_sec, width05);
-        // printf("%d %s %d",((width - word_size) >> 1) + word_size, x, ((width - word_size) >> 1) + ((width - word_size) & 1), ' ');
+//     // while (heigth == 21 & width == 92) {
+//     while(1) {
+//         system("clear");
+//         time_t t = time(NULL);
+//         struct tm* aTm = localtime(&t);
+//         printf("%02d:%02d:%02d\n", aTm->tm_hour, aTm->tm_min, aTm->tm_sec);
 
-        sleep(1);
-    }
-    // printf ("height %d\n", height);
-    // printf ("width %d\n", width);
-    return 0;  // make sure your main returns int
+//         sleep(1);
+//     }
+//     // printf ("height %d\n", height);
+//     // printf ("width %d\n", width);
+//     return 0;  // make sure your main returns int
+// }
+
+
+// SIGWINCH is called when the window is resized.
+void handle_winch(int sig){
+  signal(SIGWINCH, SIG_IGN);
+
+  // Reinitialize the window to update data structures.
+  endwin();
+  initscr();
+  refresh();
+  clear();
+
+  time_t t = time(NULL);
+  struct tm* aTm = localtime(&t);
+  // sprintf("%02d:%02d:%02d\n", aTm->tm_hour, aTm->tm_min, aTm->tm_sec);
+  // int time_size = 9; 
+
+  char tmp[128];
+  // sprintf(tmp, "%d x %d", COLS, LINES);
+  char temp[128];
+  sprintf(temp, "%02d:%02d:%02d\n  ∭", aTm->tm_hour, aTm->tm_min, aTm->tm_sec);
+
+
+  int y = LINES / 2 - 1;
+  int x = COLS / 2 - strlen(tmp) / 2;
+  // Approximate the center
+
+  mvaddstr(y, x, temp);
+  refresh();
+
+  signal(SIGWINCH, handle_winch);
+}
+
+int main(int argc, char *argv[]){
+  initscr();
+  // COLS/LINES are now set
+
+  signal(SIGWINCH, handle_winch);
+
+  while(getch() != 27){
+    /* Nada */
+  }
+
+  endwin();
+
+  return(0);
 }
